4f3bb218f71e3c56da2f35e4aaf0acc94dab1742
yaoyaodu <yaoyaodu@YYBTs-mac.local>
2019-05-30 09:14:57 +0800

    提交

---
+++ .umirc.js
@@ -0,0 +1,29 @@
+
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+  },
+  
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: false,
+      title: 'myapp1',
+      dll: false,
+      
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+}
