f7207305fdc2e5e987c962f596dc6dd21f133bea
yaoyaodu <yaoyaodu@YYBTs-mac.local>
2019-05-30 08:54:18 +0800

    提交

---
+++ src/pages/.umi/router.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Router as DefaultRouter, Route, Switch } from 'react-router-dom';
+import dynamic from 'umi/dynamic';
+import renderRoutes from 'umi/_renderRoutes';
+
+
+let Router = require('dva/router').routerRedux.ConnectedRouter;
+
+let routes = [
+  {
+    "path": "/",
+    "component": require('../../layouts/index.js').default,
+    "routes": [
+      {
+        "path": "/",
+        "exact": true,
+        "component": require('../index.js').default,
+        "_title": "myapp1",
+        "_title_default": "myapp1"
+      },
+      {
+        "component": () => React.createElement(require('/Users/yaoyaodu/Downloads/myapp/myapp1/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: false }),
+        "_title": "myapp1",
+        "_title_default": "myapp1"
+      }
+    ],
+    "_title": "myapp1",
+    "_title_default": "myapp1"
+  },
+  {
+    "component": () => React.createElement(require('/Users/yaoyaodu/Downloads/myapp/myapp1/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: false }),
+    "_title": "myapp1",
+    "_title_default": "myapp1"
+  }
+];
+window.g_routes = routes;
+window.g_plugins.applyForEach('patchRoutes', { initialValue: routes });
+
+// route change handler
+function routeChangeHandler(location, action) {
+  window.g_plugins.applyForEach('onRouteChange', {
+    initialValue: {
+      routes,
+      location,
+      action,
+    },
+  });
+}
+window.g_history.listen(routeChangeHandler);
+routeChangeHandler(window.g_history.location);
+
+export default function RouterWrapper() {
+  return (
+<Router history={window.g_history}>
+      { renderRoutes(routes, {}) }
+    </Router>
+  );
+}
